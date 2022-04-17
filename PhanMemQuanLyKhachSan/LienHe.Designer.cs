0b4a7f1871172d980e7991ee08a1b661447103b4
tranminhhoang2804 <hoangvip9x2001@gmail.com>
2022-04-17 08:44:21 +0700

    Qu-n-l-Kh-ch-s-n

---
+++ PhanMemQuanLyKhachSan/LienHe.Designer.cs
@@ -0,0 +1,314 @@
+﻿namespace PhanMemQuanLyKhachSan
+{
+    partial class frmLienHe
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbPhanMemLH = new System.Windows.Forms.Label();
+            this.lblSGOdysey = new System.Windows.Forms.Label();
+            this.lblNamPhatHanh = new System.Windows.Forms.Label();
+            this.lblPhatTrienBoi = new System.Windows.Forms.Label();
+            this.lblPTBDottedLine = new System.Windows.Forms.Label();
+            this.lblLHDottedLine = new System.Windows.Forms.Label();
+            this.lblLienHe = new System.Windows.Forms.Label();
+            this.lblTMH = new System.Windows.Forms.Label();
+            this.lblTNH = new System.Windows.Forms.Label();
+            this.lblPMH = new System.Windows.Forms.Label();
+            this.lblTKH = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnTroVeCuaLienHe = new System.Windows.Forms.Button();
+            this.lblWebsite = new System.Windows.Forms.Label();
+            this.lblFacebook = new System.Windows.Forms.Label();
+            this.lblDienThoai = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbPhanMemLH
+            // 
+            this.lbPhanMemLH.BackColor = System.Drawing.Color.Transparent;
+            this.lbPhanMemLH.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lbPhanMemLH.Font = new System.Drawing.Font("Palatino Linotype", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbPhanMemLH.Location = new System.Drawing.Point(0, 0);
+            this.lbPhanMemLH.Name = "lbPhanMemLH";
+            this.lbPhanMemLH.Size = new System.Drawing.Size(862, 42);
+            this.lbPhanMemLH.TabIndex = 0;
+            this.lbPhanMemLH.Text = "PHẦN MỀM QUẢN LÝ KHÁCH SẠN";
+            this.lbPhanMemLH.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblSGOdysey
+            // 
+            this.lblSGOdysey.BackColor = System.Drawing.Color.Transparent;
+            this.lblSGOdysey.Font = new System.Drawing.Font("Palatino Linotype", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSGOdysey.Location = new System.Drawing.Point(4, 42);
+            this.lblSGOdysey.Name = "lblSGOdysey";
+            this.lblSGOdysey.Size = new System.Drawing.Size(853, 44);
+            this.lblSGOdysey.TabIndex = 1;
+            this.lblSGOdysey.Text = "HOTEL MANAGERMENT";
+            this.lblSGOdysey.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblNamPhatHanh
+            // 
+            this.lblNamPhatHanh.AutoSize = true;
+            this.lblNamPhatHanh.BackColor = System.Drawing.Color.Transparent;
+            this.lblNamPhatHanh.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNamPhatHanh.Location = new System.Drawing.Point(644, 140);
+            this.lblNamPhatHanh.Name = "lblNamPhatHanh";
+            this.lblNamPhatHanh.Size = new System.Drawing.Size(166, 22);
+            this.lblNamPhatHanh.TabIndex = 3;
+            this.lblNamPhatHanh.Text = "Năm phát hành: 2022";
+            this.lblNamPhatHanh.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblPhatTrienBoi
+            // 
+            this.lblPhatTrienBoi.AutoSize = true;
+            this.lblPhatTrienBoi.BackColor = System.Drawing.Color.Transparent;
+            this.lblPhatTrienBoi.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPhatTrienBoi.Location = new System.Drawing.Point(12, 215);
+            this.lblPhatTrienBoi.Name = "lblPhatTrienBoi";
+            this.lblPhatTrienBoi.Size = new System.Drawing.Size(112, 22);
+            this.lblPhatTrienBoi.TabIndex = 4;
+            this.lblPhatTrienBoi.Text = "Phát triển bởi";
+            this.lblPhatTrienBoi.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblPTBDottedLine
+            // 
+            this.lblPTBDottedLine.AutoSize = true;
+            this.lblPTBDottedLine.BackColor = System.Drawing.Color.Transparent;
+            this.lblPTBDottedLine.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPTBDottedLine.Location = new System.Drawing.Point(12, 230);
+            this.lblPTBDottedLine.Name = "lblPTBDottedLine";
+            this.lblPTBDottedLine.Size = new System.Drawing.Size(145, 22);
+            this.lblPTBDottedLine.TabIndex = 5;
+            this.lblPTBDottedLine.Text = "---------------------------";
+            this.lblPTBDottedLine.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblLHDottedLine
+            // 
+            this.lblLHDottedLine.AutoSize = true;
+            this.lblLHDottedLine.BackColor = System.Drawing.Color.Transparent;
+            this.lblLHDottedLine.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLHDottedLine.Location = new System.Drawing.Point(568, 230);
+            this.lblLHDottedLine.Name = "lblLHDottedLine";
+            this.lblLHDottedLine.Size = new System.Drawing.Size(145, 22);
+            this.lblLHDottedLine.TabIndex = 6;
+            this.lblLHDottedLine.Text = "---------------------------";
+            this.lblLHDottedLine.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblLienHe
+            // 
+            this.lblLienHe.AutoSize = true;
+            this.lblLienHe.BackColor = System.Drawing.Color.Transparent;
+            this.lblLienHe.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLienHe.Location = new System.Drawing.Point(568, 215);
+            this.lblLienHe.Name = "lblLienHe";
+            this.lblLienHe.Size = new System.Drawing.Size(65, 22);
+            this.lblLienHe.TabIndex = 7;
+            this.lblLienHe.Text = "Liên hệ";
+            this.lblLienHe.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblTMH
+            // 
+            this.lblTMH.AutoSize = true;
+            this.lblTMH.BackColor = System.Drawing.Color.Transparent;
+            this.lblTMH.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTMH.Location = new System.Drawing.Point(12, 257);
+            this.lblTMH.Name = "lblTMH";
+            this.lblTMH.Size = new System.Drawing.Size(156, 22);
+            this.lblTMH.TabIndex = 8;
+            this.lblTMH.Text = "1.Trần Minh Hoàng";
+            this.lblTMH.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblTNH
+            // 
+            this.lblTNH.AutoSize = true;
+            this.lblTNH.BackColor = System.Drawing.Color.Transparent;
+            this.lblTNH.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTNH.Location = new System.Drawing.Point(14, 299);
+            this.lblTNH.Name = "lblTNH";
+            this.lblTNH.Size = new System.Drawing.Size(140, 22);
+            this.lblTNH.TabIndex = 9;
+            this.lblTNH.Text = "2.Trần Ngọc Hiếu";
+            this.lblTNH.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblPMH
+            // 
+            this.lblPMH.AutoSize = true;
+            this.lblPMH.BackColor = System.Drawing.Color.Transparent;
+            this.lblPMH.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPMH.Location = new System.Drawing.Point(14, 338);
+            this.lblPMH.Name = "lblPMH";
+            this.lblPMH.Size = new System.Drawing.Size(150, 22);
+            this.lblPMH.TabIndex = 10;
+            this.lblPMH.Text = "3.Phạm Minh Hiển";
+            this.lblPMH.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblTKH
+            // 
+            this.lblTKH.AutoSize = true;
+            this.lblTKH.BackColor = System.Drawing.Color.Transparent;
+            this.lblTKH.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTKH.Location = new System.Drawing.Point(14, 376);
+            this.lblTKH.Name = "lblTKH";
+            this.lblTKH.Size = new System.Drawing.Size(129, 22);
+            this.lblTKH.TabIndex = 11;
+            this.lblTKH.Text = "4.Trần Khắc Hải";
+            this.lblTKH.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.BackgroundImage = global::PhanMemQuanLyKhachSan.Properties.Resources.b3;
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.panel1.Location = new System.Drawing.Point(356, 119);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(136, 70);
+            this.panel1.TabIndex = 17;
+            // 
+            // btnTroVeCuaLienHe
+            // 
+            this.btnTroVeCuaLienHe.BackColor = System.Drawing.Color.DarkCyan;
+            this.btnTroVeCuaLienHe.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTroVeCuaLienHe.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnTroVeCuaLienHe.Image = global::PhanMemQuanLyKhachSan.Properties.Resources.iconBack;
+            this.btnTroVeCuaLienHe.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnTroVeCuaLienHe.Location = new System.Drawing.Point(17, 5);
+            this.btnTroVeCuaLienHe.Margin = new System.Windows.Forms.Padding(4);
+            this.btnTroVeCuaLienHe.Name = "btnTroVeCuaLienHe";
+            this.btnTroVeCuaLienHe.Size = new System.Drawing.Size(98, 40);
+            this.btnTroVeCuaLienHe.TabIndex = 16;
+            this.btnTroVeCuaLienHe.Text = "Trở về";
+            this.btnTroVeCuaLienHe.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnTroVeCuaLienHe.UseVisualStyleBackColor = false;
+            this.btnTroVeCuaLienHe.Click += new System.EventHandler(this.btnTroVeCuaLienHe_Click);
+            // 
+            // lblWebsite
+            // 
+            this.lblWebsite.BackColor = System.Drawing.Color.Transparent;
+            this.lblWebsite.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWebsite.Image = global::PhanMemQuanLyKhachSan.Properties.Resources.iconWeb;
+            this.lblWebsite.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lblWebsite.Location = new System.Drawing.Point(570, 376);
+            this.lblWebsite.Name = "lblWebsite";
+            this.lblWebsite.Size = new System.Drawing.Size(222, 38);
+            this.lblWebsite.TabIndex = 15;
+            this.lblWebsite.Text = "www.website.com";
+            this.lblWebsite.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblWebsite.Click += new System.EventHandler(this.lblWebsite_Click);
+            // 
+            // lblFacebook
+            // 
+            this.lblFacebook.BackColor = System.Drawing.Color.Transparent;
+            this.lblFacebook.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFacebook.Image = global::PhanMemQuanLyKhachSan.Properties.Resources.iconFacebook;
+            this.lblFacebook.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lblFacebook.Location = new System.Drawing.Point(568, 338);
+            this.lblFacebook.Name = "lblFacebook";
+            this.lblFacebook.Size = new System.Drawing.Size(145, 27);
+            this.lblFacebook.TabIndex = 14;
+            this.lblFacebook.Text = "facebook.com";
+            this.lblFacebook.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblDienThoai
+            // 
+            this.lblDienThoai.BackColor = System.Drawing.Color.Transparent;
+            this.lblDienThoai.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDienThoai.Image = global::PhanMemQuanLyKhachSan.Properties.Resources.iconPhone;
+            this.lblDienThoai.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lblDienThoai.Location = new System.Drawing.Point(570, 299);
+            this.lblDienThoai.Name = "lblDienThoai";
+            this.lblDienThoai.Size = new System.Drawing.Size(130, 39);
+            this.lblDienThoai.TabIndex = 13;
+            this.lblDienThoai.Text = "0796824757";
+            this.lblDienThoai.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.BackColor = System.Drawing.Color.Transparent;
+            this.lblEmail.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEmail.Image = global::PhanMemQuanLyKhachSan.Properties.Resources.iconEmail;
+            this.lblEmail.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lblEmail.Location = new System.Drawing.Point(570, 257);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(130, 27);
+            this.lblEmail.TabIndex = 12;
+            this.lblEmail.Text = "@gmail.com";
+            this.lblEmail.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // frmLienHe
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(862, 491);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnTroVeCuaLienHe);
+            this.Controls.Add(this.lblWebsite);
+            this.Controls.Add(this.lblFacebook);
+            this.Controls.Add(this.lblDienThoai);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.lblTKH);
+            this.Controls.Add(this.lblPMH);
+            this.Controls.Add(this.lblTNH);
+            this.Controls.Add(this.lblTMH);
+            this.Controls.Add(this.lblLienHe);
+            this.Controls.Add(this.lblLHDottedLine);
+            this.Controls.Add(this.lblPhatTrienBoi);
+            this.Controls.Add(this.lblNamPhatHanh);
+            this.Controls.Add(this.lblSGOdysey);
+            this.Controls.Add(this.lbPhanMemLH);
+            this.Controls.Add(this.lblPTBDottedLine);
+            this.Font = new System.Drawing.Font("Palatino Linotype", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "frmLienHe";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Liên Hệ";
+            this.Load += new System.EventHandler(this.FrmLienHe_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbPhanMemLH;
+        private System.Windows.Forms.Label lblSGOdysey;
+        private System.Windows.Forms.Label lblNamPhatHanh;
+        private System.Windows.Forms.Label lblPhatTrienBoi;
+        private System.Windows.Forms.Label lblPTBDottedLine;
+        private System.Windows.Forms.Label lblLHDottedLine;
+        private System.Windows.Forms.Label lblLienHe;
+        private System.Windows.Forms.Label lblTMH;
+        private System.Windows.Forms.Label lblTNH;
+        private System.Windows.Forms.Label lblPMH;
+        private System.Windows.Forms.Label lblTKH;
+        private System.Windows.Forms.Label lblWebsite;
+        private System.Windows.Forms.Label lblFacebook;
+        private System.Windows.Forms.Label lblDienThoai;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Button btnTroVeCuaLienHe;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
